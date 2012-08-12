5dfbb952d1e708a6c31241246fceb94cb80600dc
ctruman <cleetruman@gmail.com>
2012-08-11 23:32:05 -0700

    w

--- InsiderPagesKit/Models/IPKPage.h
+++ InsiderPagesKit/Models/IPKPage.h
@@ -12,23 +12,24 @@
 
 @interface IPKPage : IPKAbstractModel
 
-@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSDate * createdAt;
 @property (nonatomic, retain) NSString * description_text;
 @property (nonatomic, retain) NSNumber * id;
 @property (nonatomic, retain) NSString * image_content_type;
 @property (nonatomic, retain) NSString * image_file_name;
 @property (nonatomic, retain) NSString * image_file_size;
 @property (nonatomic, retain) NSDate * image_updated_at;
+@property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSNumber * privacy_setting;
+@property (nonatomic, retain) NSNumber * remoteID;
 @property (nonatomic, retain) NSNumber * sequence;
 @property (nonatomic, retain) NSNumber * sort;
-@property (nonatomic, retain) NSDate * updated_at;
+@property (nonatomic, retain) NSDate * updatedAt;
 @property (nonatomic, retain) NSNumber * user_id;
-@property (nonatomic, retain) NSDate * created_at;
-@property (nonatomic, retain) NSOrderedSet *providers;
-@property (nonatomic, retain) IPKUser *owner;
+@property (nonatomic, retain) NSNumber * is_favorite;
 @property (nonatomic, retain) NSSet *followers;
-@property (nonatomic, retain) NSNumber *is_favorite;
+@property (nonatomic, retain) IPKUser *owner;
+@property (nonatomic, retain) NSOrderedSet *providers;
 
 -(NSDictionary*)packToDictionary;
 
@@ -36,19 +37,20 @@
 
 @interface IPKPage (CoreDataGeneratedAccessors)
 
-- (void)insertObject:(NSManagedObject *)value inProvidersAtIndex:(NSUInteger)idx;
-- (void)removeObjectFromProvidersAtIndex:(NSUInteger)idx;
-- (void)insertProviders:(NSArray *)value atIndexes:(NSIndexSet *)indexes;
-- (void)removeProvidersAtIndexes:(NSIndexSet *)indexes;
-- (void)replaceObjectInProvidersAtIndex:(NSUInteger)idx withObject:(NSManagedObject *)value;
-- (void)replaceProvidersAtIndexes:(NSIndexSet *)indexes withProviders:(NSArray *)values;
-- (void)addProvidersObject:(NSManagedObject *)value;
-- (void)removeProvidersObject:(NSManagedObject *)value;
-- (void)addProviders:(NSOrderedSet *)values;
-- (void)removeProviders:(NSOrderedSet *)values;
 - (void)addFollowersObject:(IPKUser *)value;
 - (void)removeFollowersObject:(IPKUser *)value;
 - (void)addFollowers:(NSSet *)values;
 - (void)removeFollowers:(NSSet *)values;
+
+- (void)insertObject:(IPKProvider *)value inProvidersAtIndex:(NSUInteger)idx;
+- (void)removeObjectFromProvidersAtIndex:(NSUInteger)idx;
+- (void)insertProviders:(NSArray *)value atIndexes:(NSIndexSet *)indexes;
+- (void)removeProvidersAtIndexes:(NSIndexSet *)indexes;
+- (void)replaceObjectInProvidersAtIndex:(NSUInteger)idx withObject:(IPKProvider *)value;
+- (void)replaceProvidersAtIndexes:(NSIndexSet *)indexes withProviders:(NSArray *)values;
+- (void)addProvidersObject:(IPKProvider *)value;
+- (void)removeProvidersObject:(IPKProvider *)value;
+- (void)addProviders:(NSOrderedSet *)values;
+- (void)removeProviders:(NSOrderedSet *)values;
 
 @end
