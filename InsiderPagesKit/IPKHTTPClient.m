1bc6e1a834aad8c2e3ebf4b79d31dce5cf24f858
ctruman <cleetruman@gmail.com>
2012-10-12 14:17:39 -0700

    w

--- InsiderPagesKit/IPKHTTPClient.m
+++ InsiderPagesKit/IPKHTTPClient.m
@@ -553,9 +553,13 @@ static BOOL __developmentMode = NO;
                             nil];
     NSString * urlString = [NSString stringWithFormat:@"teams/%@/providers", pageId];
     [self postPath:urlString parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        IPKPage * page = [IPKPage objectWithRemoteID:@([pageId longLongValue])];
+        IPKUser * currentUser = [IPKUser currentUserInContext:[NSManagedObjectContext MR_contextForCurrentThread]];
+        IPKPage * page = [IPKPage existingObjectWithRemoteID:@([pageId longLongValue])];
         IPKProvider * providerToAdd = provider;
-        IPKTeamMembership * teamMembership = [IPKTeamMembership teamMembershipForUserID:[IPKUser currentUserInContext:[NSManagedObjectContext MR_contextForCurrentThread]].remoteID teamID:page.remoteID listingID:providerToAdd.remoteID];
+        IPKTeamMembership * teamMembership = [IPKTeamMembership createMembershipForUserID:currentUser.remoteID teamID:page.remoteID listingID:providerToAdd.remoteID];
+        teamMembership.position = @(1);
+        teamMembership.pollaverage = @(NO);
+        [[NSManagedObjectContext MR_contextForCurrentThread] MR_save];
         
         [[NSManagedObjectContext MR_contextForCurrentThread] MR_save];
         
@@ -581,6 +585,12 @@ static BOOL __developmentMode = NO;
         IPKProvider * providerToAdd = provider;
         IPKTeamMembership * teamMembership = [IPKTeamMembership createMembershipForUserID:currentUser.remoteID teamID:page.remoteID listingID:providerToAdd.remoteID];
         teamMembership.position = @(1);
+        teamMembership.pollaverage = @(NO);
+        NSMutableArray * teamMemberships = [[IPKTeamMembership MR_findByAttribute:@"owner_id" withValue:currentUser.remoteID inContext:[NSManagedObjectContext MR_contextForCurrentThread]] mutableCopy];
+        [teamMemberships removeObject:teamMembership];
+        for (IPKTeamMembership * tm in teamMemberships) {
+            tm.position = @(tm.position.intValue + 1);
+        }
         [[NSManagedObjectContext MR_contextForCurrentThread] MR_save];
         
         if (success) {
