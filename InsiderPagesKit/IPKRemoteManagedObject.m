455ae9a640c0159b8513987dea14af327a3b40c8
ctruman <cleetruman@gmail.com>
2012-10-22 17:31:40 -0700

    w

--- InsiderPagesKit/IPKRemoteManagedObject.m
+++ InsiderPagesKit/IPKRemoteManagedObject.m
@@ -257,7 +257,8 @@
 
 
 - (BOOL)shouldUnpackDictionary:(NSDictionary *)dictionary {
-	return self.updatedAt == nil || [self.updatedAt isEqualToDate:[[self class] parseDate:[dictionary objectForKey:@"updated_at"]]] == NO;
+//	return self.updatedAt == nil || [self.updatedAt isEqualToDate:[[self class] parseDate:[dictionary objectForKey:@"updated_at"]]] == NO;
+    return YES;
 }
 
 
